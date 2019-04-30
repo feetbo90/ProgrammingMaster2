ff28990b26d92b63bb0deddfccab4cdee304fc1e
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-04-30 17:11:56 +0700

    Penjumlahan android

--- app/src/main/java/master/programming/programmingmaster/MainActivity.kt
+++ app/src/main/java/master/programming/programmingmaster/MainActivity.kt
@@ -2,11 +2,26 @@ package master.programming.programmingmaster
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.penjumlahan.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var bilSatu : Int = 0
+    private var bilDua : Int = 0
+    private var has : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.penjumlahan)
+
+
+        jumlah.setOnClickListener {
+            bilSatu = bilanganSatu.text.toString().toInt()
+            bilDua = bilanganDua.text.toString().toInt()
+            has = bilSatu + bilDua
+            hasil.setText("$has")
+        }
+
+
     }
 }
